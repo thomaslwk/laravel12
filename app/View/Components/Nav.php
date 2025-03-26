7e3f12559b995273cbba220ff15ccfd761ac646b
Thomas Lee <thomaslwk1@gmail.com>
2025-03-26 17:10:58 +0800

    update: 2025-03-26 17:10:58

---
+++ app/View/Components/Nav.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class Nav extends Component
+{
+    public function render()
+    {
+        return view('Components.nav-link');
+    }
+}
---
+++ app/View/Components/NavLink.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class NavLink extends Component
+{
+    public function render()
+    {
+        return view('Components.nav-link');
+    }
+}
